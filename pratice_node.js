2b1d95fb66840fbd14f799b930161bfc7b5e3f91
pawanvaidya32 <vaidyapawan861@gmail.com>
2024-06-26 16:41:52 +0530

    working

---
+++ pratice_node.js
@@ -0,0 +1,101 @@
+const pratice2 = require('./pratice2')
+// console.log(pratice2.z())
+
+ arr = [5,8,3,4,5,3,8,4,5,8]
+let arry = arr.filter((item)=>{
+    return item > 5
+})
+
+// console.log(arry)
+// const fs= require('fs').writeFileSync('pawan.text','hello pawan')
+// console.log('------>', __filename);
+// fs.writeFileSync('pawan.text', 'hello pawan')
+
+
+// const http = require('http');
+
+// http.createServer((req, resp)=>{
+//     resp.write('<h1>Hello</h1>');
+//     resp.end();
+// }).listen(4333);
+
+
+
+// const mat =(a)=>a*10
+// console.log(mat(2));
+// console.log(mat(3));
+
+
+
+// const pttp = require('http')
+// const data = require('./data')
+
+// pttp.createServer((req,resp)=>{
+//     resp.writeHead(200,{'Content-Type':'application\JSON '});
+//     resp.write(JSON.stringify(data));
+//     resp.end();
+// }).listen(5000);
+
+// ------------------------------
+
+// console.log(process.argv[5])
+
+// const fs = require('fs')
+// const input = process.argv;
+
+// if(input[2] == 'add')
+//     {
+//         fs.writeFileSync(input[3], input[4])
+//     } else if(input[2]=="remove") 
+//     {
+//         fs.unlinkSync(input[3])
+//     }
+//     else{
+//         console.log("invail output")
+//     }
+
+// ------------------------------
+
+// const fs= require('fs');
+// const path = require('path');
+//  const dirpath=path.join(__dirname,'files');
+
+// for(i=0;i<4;i++){
+//     fs.writeFileSync(`${dirpath}/hello${i}.txt`,`a simple text file ${i}`)
+// }
+
+// fs.readdir(dirpath,(err,files)=>{
+//     console.warn(files)
+// })
+
+// fs.readdir(dirpath,(err,files)=>{
+//     files.forEach((item)=>{
+//          console.log("file name is",item)
+//     })
+// })
+
+ary=[1,2,3]
+// ary.forEach((item)=>{console.log("hello",item)})
+// ----------------------------------------
+
+const fs = require('fs');
+const path = require('path');
+const dirpath = path.join(__dirname,'crud');
+
+const filepath = `${dirpath}/apple.txt`
+
+// fs.writeFileSync(filepath,"apple file.text one")
+// fs.readFile(filepath,'utf-8',(err,item)=>{
+//     console.log(item)
+// })
+// fs.appendFile(filepath,'hello pawan',(err)=>{
+//     if(!err)console.log("update the file ")
+// })
+
+// fs.rename(filepath,`${dirpath}/pawan.txt`,(err)=>{
+//     if(!err)console.log("file name is upadate")
+// })
+
+// fs.unlinkSync(`${dirpath}/pawan.txt`)
+
+// -----------------------------------------
